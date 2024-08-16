53593ffa98a05de4099b51db73a52b66906ea1e7
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-08-16 23:39:13 +0100

    Add files via upload

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './MainPage.css'
+
+const MainPage = () => {
+    return (
+        <div>
+            <h1> U TEACH MI </h1>
+        </div>
+    );
+};
+
+export default MainPage;
