572c1e6a1d17bda6a4617d558f389d3b084eac05
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-08-24 20:48:44 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
 import './App.css'
+import Footer from './components/footer/Footer'
 import MainPage from './pages/MainPage'
 
 function App() {
   return (
+    <>
   <MainPage />
+  <Footer />
+  </>
+  
 )
 }
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './MainPage.css'
-import Header from "../components/header/header";
+import Header from "../components/header/Header";
 
 const MainPage = () => {
     return (
