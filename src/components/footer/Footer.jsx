9081552fe9949da8d780aac547195ede7b2aa1e9
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-08-24 20:48:06 +0100

    Add files via upload

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import "./footer.css";
+
 
 const Footer = () => {
     return (
-        <div>
+        <div className="footer">
+            <div className="wrapper">
+                asdasd
+            </div>
             
         </div>
     )
