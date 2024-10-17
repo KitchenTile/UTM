342d4875da0026ead5381ab69967d45db4de5b7b
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-10-17 09:38:20 +0100

    Add files via upload

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,20 +1,31 @@
-import React from "react";
-import './MainPage.css'
-import Header from "../components/mainpagecomponents/header/Header";
-import Hero from "../components/mainpagecomponents/hero/hero";
-import Ourservices from "../components/mainpagecomponents/ourservices/OurServices";
-import Moreonus from "../components/mainpagecomponents/moreonus/Moreonus";
+import './MainPage.css';
 
+import Header from '../components/main-page/header/Header';
+import Hero from '../components/main-page/hero/Hero';
+import OurServices from '../components/main-page/ourservices/OurServices';
+import MoreOnUs from '@/components/main-page/moreonus/Moreonus';
+import FindSchools from '@/components/main-page/findschools/Findschools';
+import Testimonial from '@/components/main-page/testimonial/Testimonial';
+import Techadventure from '@/components/main-page/techadventure/Techadventure';
+import Carousel from '../components/main-page/carousel/Carousel';
+import Form from '@/components/main-page/form/Form';
+import Footer from '../components/main-page/footer/Footer';
 
 const MainPage = () => {
-    return (
-        <>
-            <Header />
-            <Hero />
-            <Ourservices />
-            <Moreonus />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Hero />
+      <OurServices />
+      <MoreOnUs />
+      <FindSchools />
+      <Testimonial />
+      <Techadventure />
+      <Carousel />
+      <Form />
+      <Footer />
+    </>
+  );
 };
 
 export default MainPage;
